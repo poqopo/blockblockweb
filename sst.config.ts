45312c4714cfebc0df7f79b69e0e03bac22a529e
Yoonhyoung-Lee <54613623+Yoonhyoung-Lee@users.noreply.github.com>
2024-03-05 17:24:06 +0900

    Add sst

---
+++ sst.config.ts
@@ -0,0 +1,28 @@
+import { SSTConfig } from 'sst';
+import { StaticSite } from 'sst/constructs';
+
+export default {
+  config(_input) {
+    return {
+      name: 'blockblockweb',
+      region: 'ap-northeast-2',
+    };
+  },
+  stacks(app) {
+    app.stack(function Site({ stack }) {
+      const site = new StaticSite(stack, 'react', {
+        buildOutput: 'dist',
+        customDomain: {
+          domainName:
+            stack.stage === 'prod'
+              ? 'ysblockblock.com'
+              : 'dev.ysblockblock.com',
+          hostedZone: 'ysblockblock.com',
+        },
+      });
+      stack.addOutputs({
+        SiteUrl: site.url,
+      });
+    });
+  },
+} satisfies SSTConfig;
